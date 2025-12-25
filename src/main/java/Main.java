54374dc403640ead3641232cef3561a66a75d5f9
Aya Ron <77265207+ayaronn@users.noreply.github.com>
2025-12-25 09:33:47 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,107 +1,106 @@
 import java.util.*;
 public class Main {
 	public static boolean numC(Queue <Integer>q,int x) {
+		Queue <Integer> q4=copy(q);
 		
-		while(!q.isEmpty()) {
-			if(q.remove()==x) {
+		while(!q4.isEmpty()) {
+			if(q4.remove()==x) {
 				return true;
 			}
 		}
 		return false;
 	}
-	public static Queue<Integer> copy(Queue <Integer> q){
-		Queue <Integer> newQ1 =new Queue<>();
-		Queue <Integer> newQ2 =new Queue<>();
+	public static Queue<Integer> copy(Queue<Integer> q) {
+	    Queue<Integer> newQ = new Queue<>();
+	    Queue<Integer> tempQ = new Queue<>();
+	    
+	    while (!q.isEmpty()) {
+	        int element = q.remove(); 
+	        tempQ.insert(element);     
+	        newQ.insert(element);     
+	    }
 
-		while(!q.isEmpty()) {
-			newQ2.insert(q.head());
-			newQ2.insert(q.remove());
-			
-		}
-		while(!newQ1.isEmpty()) {
-			q.insert(newQ1.remove());
-		}
-		return newQ2;
+	    while (!tempQ.isEmpty()) {
+	        q.insert(tempQ.remove()); 
+	    }
 
-		
+	    return newQ;  
 	}
+
+
 	public static double avg(Queue <Integer> q) {
 		Queue <Integer> q2=copy(q);
 
+		System.out.println(q);
 		int sum=0;
 		int count=0;
-		while(!q2.isEmpty()) {
-			sum+=sum=q2.remove();
+		while(!q2.isEmpty()) 
+		{
+			sum+=q2.remove();
 			count++;
 		}
 		return sum/count;
 	}
 	
-	public static boolean allDivide(Queue<Integer> q1, Queue<Integer> q2) {
-    Queue<Integer> temp1 = new Queue<>();
-    Queue<Integer> temp2 = new Queue<>();
-
-    boolean divides;
-
-    while (!q1.isEmpty()) {
-        int x = q1.remove();
-        temp1.insert(x);
-        divides = false;
-
-        while (!q2.isEmpty()) {
-            int y = q2.remove();
-            temp2.insert(y);
-
-            if (y % x == 0) {
-                divides = true;
-            }
-        }
-
-        while (!temp2.isEmpty()) {
-            q2.insert(temp2.remove());
-        }
-
-        if (!divides) {
-            while (!temp1.isEmpty()) {
-                q1.insert(temp1.remove());
-            }
-            return false;
-        }
-    }
-
-    while (!temp1.isEmpty()) {
-        q1.insert(temp1.remove());
-    }
-
-    return true;
-}
-public static boolean appears(Queue<Integer> q, int val) 
+	public static boolean allDivide(Queue<Integer> q1, Queue<Integer> q2) 
 	{
-    Queue<Integer> temp = new Queue<>();
-    int count = 0;
-
-    while (!q.isEmpty()) {
-        int x = q.remove();
-        temp.insert(x);
-
-        if (x == val) {
-            count++;
-            if (count == 2) {
-                while (!temp.isEmpty()) {
-                    q.insert(temp.remove());
-                }
-                return true;
-            }
-        } else {
-            count = 0;
-        }
-    }
-
-    while (!temp.isEmpty()) {
-        q.insert(temp.remove());
-    }
-
-    return false;
+		Queue <Integer> copy1=copy(q1);
+		Queue <Integer> copy2=copy(q2);
+	    
+	    boolean divides;
+	
+	    while (!copy1.isEmpty()) 
+	    {
+	        int x = copy1.remove();
+	        divides = false;
+	
+	        while (!copy2.isEmpty()) 
+	        {
+	            int y = copy2.remove();
+	            if (y % x == 0) 
+	            {
+	                divides = true;
+	            }
+	        }
+	        if (!divides) 
+	        {
+	        	return false;
+	        }
+	    }
+	    return true;
+	}
+	
+	
+	
+	
+	
+	public static boolean appears(Queue<Integer> q, int val) 
+	{
+	    Queue<Integer> temp = new Queue<>();
+	    int count = 0;
+	
+	    while (!q.isEmpty()) {
+	        int x = q.remove();
+	        temp.insert(x);
+	
+	        if (x == val) {
+	            count++;
+	            if (count == 2) {
+	                while (!temp.isEmpty()) {
+	                    q.insert(temp.remove());
+	                }
+	                return true;
+	            }
+	        } else {
+	            count = 0;
+	        }
+	    }
+	
+	    while (!temp.isEmpty()) {
+	        q.insert(temp.remove());
+	    }
+	
+	    return false;
 }
 
 
@@ -115,7 +114,8 @@ public static boolean appears(Queue<Integer> q, int val)
 		System.out.println(numC(q,8));
 		System.out.println(q);
 		System.out.println(avg(q));
-		
+		System.out.println(allDivide(q,q));
+		System.out.println(appears(q,8));
 	}
 
 }
